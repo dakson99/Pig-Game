2fc739a4088d17a1004d2c71158f9d317a831749
Darko <72525936+dakson99@users.noreply.github.com>
2024-06-21 19:18:09 +0200

    Resetting the Game button

--- script.js
+++ script.js
@@ -14,14 +14,27 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const score = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -56,13 +69,13 @@ btnRoll.addEventListener('click', function () {
 
 btnHold.addEventListener('click', function () {
   if (playing) {
-    score[activePlayer] += currentScore;
-    console.log(score[activePlayer]);
+    scores[activePlayer] += currentScore;
+    console.log(scores[activePlayer]);
 
     document.getElementById(`score--${activePlayer}`).textContent =
-      score[activePlayer];
+      scores[activePlayer];
 
-    if (score[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 20) {
       playing = false;
       diceEl.classList.add('hidden');
       document
@@ -76,3 +89,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
